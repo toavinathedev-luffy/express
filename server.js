e932a96caf04751d689667612dd9a64a56085efe
toavinathedev_luffy <toavinathefirst@gmail.com>
2022-09-14 18:43:18 +0300

    salut

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const errorHandler = error => {
     if (error.syscall !== 'listen') throw error;
 
 const address = server.address();
-const bind = typeof address === 'string' ? 'pipe' + adress : 'port:' + port;
+const bind = typeof address === 'string' ? 'pipe' + address : 'port:' + port;
 switch (error.code) {
     case 'EACCES':
         console.error(bind + 'requires elevated privileges.');
+
         process.exit(1);
         break;
     case 'EADDRINUSE':
